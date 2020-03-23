bfd6572162bf28bd43298580bf76e0c61ad904a7
Dave Gregory <dave.gregory@simplyhealth.co.uk>
2020-03-23 13:47:52 +0000

    Update and rename javascript_to_review.rtf to javascript_to_review.js
    pasted in code

---
+++ javascript_to_review.js
@@ -0,0 +1,23 @@
+const houseNumbers = [20, 22, 10, 4]
+const emailaddrs = ['dave@somemail.com’, 'bob@domain.com’, 'anthony@somemail.com’];
+function combineDataToObjects() {
+  let person = [];
+  for (var x = 0; x <= names.length; x++) {
+    const person = {
+      name: names[x],
+      houseNumber: houseNumbers[x],
+      email: emailaddrs[x]
+    };
+    person.unshift(person)
+  }
+  return person;
+}
+function show(persons) {
+  for (const p in persons) {
+    console.log(
+      “Client: ” + p.name + ` who lives at number: ${p.houseNumber} ` + “and can be contacted at: ” + p.email
+    );
+  }
+}
+combineDataToObjects();
+show(persons);
